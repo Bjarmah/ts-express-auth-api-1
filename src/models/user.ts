47dddd3b3b43975fb42469de3e085ff3cfd6cf6d
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-14 12:21:38 +0000

    Update user.ts
    added user model details

---
+++ src/models/user.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import bcryt from 'bcrypt';
+
+export enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user',
+    GUEST = 'guest'
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.USER
+    })
+    role: UserRole;
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcryt.hash(this.password, 10);
+    }
+
+
+
+}
