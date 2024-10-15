5d17ab2184c13ea6481e36f7c7028c52ace19f35
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-15 21:46:06 +0000

    Update userController.ts
    Added a getProfile controller that collects gets the current profile using the userID it gets from the token

---
+++ src/controllers/userController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/user";
+
+
+export const getProfile = async (req: Request, res: Response, next: NextFunction) => {
+    const userRepository = getRepository(User);
+    const userID = req.user?.id;
+
+    try {
+        const users = await userRepository.findOne({ where: { id: userID } });
+        if (!users) {
+            return res.status(404).send("User not found");
+        }
+        res.json({ user: { id: users.id, email: users.email, role: users.role } });
+    } catch (error) {
+        res.status(400).send("Error fetching users");
+    }
+
+}
+
