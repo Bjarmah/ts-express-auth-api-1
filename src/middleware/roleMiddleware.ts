ba1017d5a0dcc8fbcd95d8f651942d183075106d
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-16 19:39:33 +0000

    Update roleMiddleware.ts
    reconfigured middleware to allow sharing of resources across multiple roles

--- src/middleware/roleMiddleware.ts
+++ src/middleware/roleMiddleware.ts
@@ -1,13 +1,34 @@
 import { Request, Response, NextFunction } from "express";
-import { UserRole } from "../models/user";
+import { User, UserRole } from "../models/user";
 
-export const checkRole = (role: UserRole) => {
+// export const checkRole = (role: UserRole) => {
+//     return (req: Request, res: Response, next: NextFunction) => {
+//         if (req.user && req.user.role === role) {
+//             next();
+//         } else {
+//             res.sendStatus(403).json({ message: 'Unauthorized, Access Denied' });
+//         }
+//     };
+// };
+
+export const checkRole = (allowedRoles: UserRole[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (req.user && req.user.role === role) {
-            next();
-        } else {
-            res.sendStatus(403).json({ message: 'Unauthorized, Access Denied' });
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized - No user found' });
         }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: 'Forbidden - You do not have permission to access this resource'
+            });
+        }
+
+        next();
     };
 };
+
+
+export const isAdmin = checkRole([UserRole.ADMIN]);
+export const isUser = checkRole([UserRole.USER]);
+export const isGuest = checkRole([UserRole.GUEST]);
 
