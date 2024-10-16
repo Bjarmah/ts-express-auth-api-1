cd117448b6cbf131caaf40586af9ca41705cd038
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-16 21:12:23 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
 
 
 const app = express();
@@ -9,6 +11,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-
-
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 export default app;
