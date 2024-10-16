937b938ff5308775301a6412d6b5632c9849df69
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-16 21:01:08 +0000

    Update authMiddleware.ts

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -3,27 +3,32 @@ import { getRepository } from 'typeorm';
 import { User } from '../models/user';
 import { verifyToken } from '../utils/jwt';
 
-export const authenticateJWT = async (req: Request, res: Response, next: NextFunction) => {
+export const authenticateJWT = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
     const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-
-        try {
-            const payload = verifyToken(token);
-            const userRepository = getRepository(User);
-            const user = await userRepository.findOne(payload.id);
-
-            if (user) {
-                req.user = user;
-                next();
-            } else {
-                res.sendStatus(401);
-            }
-        } catch (error) {
-            return res.sendStatus(403);
-        }
-    } else {
+    if (!authHeader) {
         res.sendStatus(401);
+        return;
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const payload = verifyToken(token);
+        const userRepository = getRepository(User);
+        const user = await userRepository.findOne(payload.id);
+
+        if (user) {
+            req.user = user;
+            next();
+        } else {
+            res.sendStatus(401);
+        }
+    } catch (error) {
+        res.sendStatus(403);
     }
 };
