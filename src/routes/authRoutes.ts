9553259e5d5dbd1a61dd1991ea6c8419339b1159
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-18 20:40:05 +0000

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,19 +2,21 @@ import express from 'express';
 import {
     register,
     login,
-    assignRole
+    assignRole, verifyOTP
 } from '../controllers/authController';
 import { authenticateJWT } from '../middleware/authMiddleware';
 import { isAdmin } from '../middleware/roleMiddleware';
+import { loginLimiter } from '../middleware/rate-limit';
 
 const router = express.Router();
 
 // Public routes (no authentication required)
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', loginLimiter, register as any);
+router.post('/login', loginLimiter, login as any);
+router.post('/verifyOTP', verifyOTP as any);
 
 
 // Protected routes
-router.post('/assign-role', authenticateJWT, isAdmin, assignRole); // Only admins can assign roles
+router.post('/assign-role', authenticateJWT, isAdmin, assignRole as any); // Only admins can assign roles
 
 export default router;
