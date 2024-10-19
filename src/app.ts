d207b9f0e2ad60f16fe260170bfdd49b2f2ccc3c
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-19 21:38:58 +0000

    Swagger implementation
    Setting up project for swagger

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,23 @@ import cors from 'cors';
 import helmet from 'helmet';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
-
-
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 const app = express();
+
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml')); // adjust path as needed
+
+// Add Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 app.use(helmet());
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.xssFilter());
 app.use(express.json());
+
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
