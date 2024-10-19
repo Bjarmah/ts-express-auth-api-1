faea8ca229eda86c1717778082efff2bc7223efb
Benedict Jason Armah <bjarmah1@gmail.com>
2024-10-19 08:40:09 +0000

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,15 +4,24 @@ import { OTP } from "../models/otp";
 
 const config: DataSourceOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'intern_api',
+    ...(process.env.NODE_ENV === 'production'
+        ? {
+            url: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            },
+        }
+        : {
+            host: process.env.DB_HOST || 'localhost',
+            port: Number(process.env.DB_PORT) || 5432,
+            username: process.env.DB_USER || 'postgres',
+            password: process.env.DB_PASSWORD || 'admin',
+            database: process.env.DB_NAME || 'intern_api',
+        }
+    ),
     entities: [User, OTP],
-    synchronize: process.env.NODE_ENV === 'development',
+    synchronize: process.env.NODE_ENV === 'development', // Be careful with this in production
     logging: false,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 }
 
 const AppDataSource = new DataSource(config);
